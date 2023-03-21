e433aae2a6cbb0b5c514b32369395ce32b538417
Alexjaca <jalexcardenasa@gmail.com>
2023-03-21 19:07:00 +0100

    Publications

--- controllers/publication.js
+++ controllers/publication.js
@@ -1,11 +1,78 @@
+const Publication = require("../models/publication");
+
 //acciones de prueba
-const pruebaPublication = (req, res) =>{
+const pruebaPublication = (req, res) => {
     return res.status(200).json({
         message: "Estamos en la Ruta del controlador pruebaPublication"
     });
 }
 
+//GUARDAR PUBLICACION/////////////////////////////////////////////////////////////////////////
+const save = (req, res) => {
+
+    //recoger datos del body
+    const params = req.body;
+
+    //si no llegan dar respuesta negativa
+    if (!params.text) return res.status(400).send({ status: "error", message: "No Ingreso Ninguna Publicacion" });
+
+    //crear y rellenar el objeto del modelo
+    let publication = new Publication(params);
+    publication.user = req.user.id;
+
+    //guartdar objeto en bbdd
+    publication.save((err, publicationStored) => {
+        if (err || !publicationStored) {
+            return res.status(404).send({ status: "error", message: "No se Guardo la Publicacion" });
+        }
+
+        return res.status(200).json({
+            status: "success",
+            message: "Publicacion Guardada",
+            publication: publicationStored
+        });
+    });
+
+}
+
+//SACAR UNA PUBLICACION EN CONCRETO/////////////////////////////////////////////////////////////////////////
+const detail = (req, res) => {
+
+    //Sacar id de publicacion de la url
+    const publicationId = req.params.id;
+
+    //find con la condicion del id
+    Publication.findById(publicationId, (err, publicationStored) => {
+
+        if(err || !publicationStored){
+            return res.status(404).send({ status: "error", message: "No existe la publicacion" });
+        }
+
+        //devolver respuesta
+        return res.status(200).json({
+            status: "success",
+            message: "Controlador de Detalle de publicacion",
+            publication: publicationStored
+        });
+
+    });
+
+
+}
+
+//LISTAR TODAS LAS PUBLICACIONES/////////////////////////////////////////////////////////////////////////
+
+//LISTAR PUBLICACIONES DE UN USUARIO EN CONCRETO/////////////////////////////////////////////////////////////////////////
+
+//ELIMINAR PUBLICACIONES/////////////////////////////////////////////////////////////////////////
+
+//SUBIR FICHEROS/////////////////////////////////////////////////////////////////////////
+
+//DEVOLVER ARCHIVOS M,ULTIMEDIA/////////////////////////////////////////////////////////////////////////
+
 //exportar acciones
 module.exports = {
-    pruebaPublication
+    pruebaPublication,
+    save,
+    detail
 }
--- routes/publication.js
+++ routes/publication.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const PublicationController = require("../controllers/publication");
+const authorization = require("../middlewares/auth");
 
 //Rutas
 router.get("/prueba-publication", PublicationController.pruebaPublication);
+router.post("/save", authorization.auth, PublicationController.save);
+router.get("/detail/:id", authorization.auth, PublicationController.detail);
 
 
 //esportar router para ser usado en la app
