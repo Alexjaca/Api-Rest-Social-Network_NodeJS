c20dddade3474b7a86911aaed836f5dd46e919e9
Alexjaca <jalexcardenasa@gmail.com>
2023-03-21 21:46:30 +0100

    find publications

--- controllers/publication.js
+++ controllers/publication.js
@@ -44,27 +44,87 @@ const detail = (req, res) => {
     //find con la condicion del id
     Publication.findById(publicationId, (err, publicationStored) => {
 
-        if(err || !publicationStored){
+        if (err || !publicationStored) {
             return res.status(404).send({ status: "error", message: "No existe la publicacion" });
         }
 
         //devolver respuesta
         return res.status(200).json({
             status: "success",
-            message: "Controlador de Detalle de publicacion",
+            message: "Mostrar Publicacion",
             publication: publicationStored
         });
 
     });
-
-
 }
+
+
+//ELIMINAR PUBLICACIONES/////////////////////////////////////////////////////////////////////////
+const remove = (req, res) => {
+
+    //Sacar el Id de la publicacion a eliminar
+    const publicationId = req.params.id;
+
+    // Find y luego remove
+    Publication.find({ "user": req.user.id, "_id": publicationId }).remove(err => {
+
+        if (err) {
+            return res.status(500).json({
+                status: "error",
+                message: "No se ha eliminado la publicacion"
+            });
+        }
+
+        //devolver respuesta
+        return res.status(200).json({
+            status: "success",
+            message: "Eliminar Publicacion",
+            publication: publicationId
+        });
+
+    });
+}
+
+//LISTAR PUBLICACIONES DE UN USUARIO EN CONCRETO//////////////////////////////////////////////////////////
+const user = (req, res) => {
+
+    //Sacar Id de ususario
+    const userId = req.params.id;
+
+    //Controlar la pagina
+    let page = 1;
+    let itemsPerPage = 4;
+    if (req.params.page) page = req.params.page;
+
+    //Find, populate, ordenar, paginar
+    Publication.find({ "user": userId })
+                .sort("-create_at")//Ordenando de la publicacion mas reciente con el (-)
+                .populate("user", '-password -__v -role')//Populate trate el objeto a travez de su id
+                .paginate(page, itemsPerPage, (err, publications, total) => {
+
+        if(err || !publications || publications.length <= 0){
+            return res.status(404).json({
+                status: "error",
+                message: "No hay publicaciones para mostrar"
+            });
+        }
+
+        //devolver resultado
+        return res.status(200).json({
+            status: "success",
+            message: "Publicaciones del perfil de un usuario",
+            page,
+            total,
+            pages: Math.ceil(total/itemsPerPage),
+            publications
+        });
+    }); 
+}
+
 
 //LISTAR TODAS LAS PUBLICACIONES/////////////////////////////////////////////////////////////////////////
 
-//LISTAR PUBLICACIONES DE UN USUARIO EN CONCRETO/////////////////////////////////////////////////////////////////////////
 
-//ELIMINAR PUBLICACIONES/////////////////////////////////////////////////////////////////////////
 
 //SUBIR FICHEROS/////////////////////////////////////////////////////////////////////////
 
@@ -74,5 +134,7 @@ const detail = (req, res) => {
 module.exports = {
     pruebaPublication,
     save,
-    detail
+    detail,
+    remove,
+    user
 }
--- routes/publication.js
+++ routes/publication.js
@@ -7,6 +7,8 @@ const authorization = require("../middlewares/auth");
 router.get("/prueba-publication", PublicationController.pruebaPublication);
 router.post("/save", authorization.auth, PublicationController.save);
 router.get("/detail/:id", authorization.auth, PublicationController.detail);
+router.delete("/remove/:id", authorization.auth, PublicationController.remove);
+router.get("/user/:id/:page?", authorization.auth, PublicationController.user);
 
 
 //esportar router para ser usado en la app
